66e97265d7b85d1d8093177259476ab495d8c7cc
Guilermo Fragachan <guillefragachan@gmail.com>
2025-04-12 12:46:42 +0200

    fix router

--- my-app/src/Navbar/Navbar.jsx
+++ my-app/src/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 // import { useState } from 'react'
 import './Navbar.css'
 import frelloLogo from './Frello.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-
+// const router = 
 return (
   <div className="navbar">
     <div className='img-logo'>
       <img className='logo' src={frelloLogo} alt="logo" /> </div>
     <nav className="navbar-cont">
       <ul className='left-panel'>
+        <Link to={"/dashboard"}>
         <li><a href="#">Dashboard</a></li>
+        </Link>
         <li><a href="#">About</a></li>
       </ul>
     </nav>
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -5,19 +5,19 @@ import Navbar from './Navbar/Navbar.jsx'
 import Mainboard from './MainBoard.jsx'
 import NotFoundPage from './NotFoundPage.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {Dashboard} from './dashboard.jsx'
 
 // npx vite
 
 const router = createBrowserRouter([
   {path: "/", element: <Navbar/>},
-  // {path: "/dashboard", element: <Dashboard/>},
+  {path: "/dashboard", Component: Dashboard},
   // {path: "/about", element: <About/>},
   {path: "*", element: <NotFoundPage/>},
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
     <RouterProvider router={router} />
       <Mainboard />
   </StrictMode>,
