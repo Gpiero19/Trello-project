d7a48bef16bbf3a2b61d8d39cea5e4b681d74d6f
Guilermo Fragachan <guillefragachan@gmail.com>
2025-04-13 17:37:24 +0200

    hhh

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Navbar from './Navbar/Navbar.jsx'
-import Mainboard from './MainBoard.jsx'
-import NotFoundPage from './NotFoundPage.jsx'
-import {  BrowserRouter, Route, Routes } from 'react-router-dom'
-import {Dashboard} from './dashboard.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Navbar from "./Navbar/Navbar.jsx";
+import Mainboard from "./MainBoard.jsx";
+import NotFoundPage from "./NotFoundPage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./dashboard.jsx";
 
 // npx vite
 
@@ -16,22 +16,16 @@ import {Dashboard} from './dashboard.jsx'
 //   {path: "*", element: <NotFoundPage/>},
 // ])
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
-      <BrowserRouter>
-      <Navbar/>
+    <BrowserRouter errorElement={<NotFoundPage/>}>
+      <Navbar />
 
       <Routes>
-      <Route path="/" element={<Mainboard />} />
-    </Routes>
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-    <Routes>
-      {/* <Route path="*" element={<NotFoundPage />} /> */}
-    </Routes>
+        <Route path="/" element={<Mainboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       </BrowserRouter>
-
-  </StrictMode>,
-)
+  </StrictMode>
+);
