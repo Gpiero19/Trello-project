2a61184e7cf73ff44dced707e22e79ee5d8da1f2
Gian Piero Canevari <canevarigian@gmail.com>
2025-04-20 00:41:38 +0200

    minor fixes notfound 404

--- my-app/src/NotFoundPage/NotFoundPage.jsx
+++ my-app/src/NotFoundPage/NotFoundPage.jsx
@@ -3,17 +3,21 @@ import './NotFoundPage.css'
 
 const NotFoundPage = () => {
     return(
-        
-        <div className="NotFoundMessage">
-            <h1>404</h1>
-            <h2>UH OH! You're lost.</h2>
-            <p>The page you are looking for does not exist.
-            How you got here is a mystery. But you can click the button below
-            to go back to the homepage.
-            </p>
-            <Link to={"/"}>
-                <button className="btn orange">Go back Home</button>
-            </Link>
+        <div className="container">
+            <div className="container-img">
+                
+            </div>
+            <div className="NotFoundMessage">
+                <h1>404</h1>
+                <h2>UH OH! You're lost.</h2>
+                <p>The page you are looking for does not exist.
+                How you got here is a mystery. But you can click the button below
+                to go back to the homepage.
+                </p>
+                <Link to={"/"}>
+                    <button className="btn orange">Go back Home</button>
+                </Link>
+            </div>
         </div>
     );
 };
