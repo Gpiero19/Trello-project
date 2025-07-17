619124328d85eab128639e5c8300fc50027eb337
Gian Piero Canevari <canevarigian@gmail.com>
2025-07-17 14:00:56 +0200

    board routes

--- my-app/server/routes/boardRoutes.js
+++ my-app/server/routes/boardRoutes.js
@@ -4,5 +4,9 @@ const boardController = require('../controllers/boardController');
 
 router.post('/', boardController.createBoard); //Post api/boards
 router.get('/', boardController.getAllBoards); //Fetch api/boards
+router.get('/:id', boardController.getBoardById); //Fetch api/boards by ID
+router.put('/', boardController.updateBoard); //Update api/boards by ID
+router.delete('/:id', boardController.deleteBoard); //Delete api/boards
+
 
 module.exports = router;
--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const boardsRoutes = require('/routes/BoardRoutes')
+const boardsRoutes = require('/routes/boardRoutes')
 require('dotenv').config();
 
 const app = express();
